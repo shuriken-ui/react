4dd3c160889a266ca95d994fb9eb78a485b0e2fc
Franklin Shera <fshera96@gmail.com>
2023-10-24 12:01:38 +0300

    fix: remove React import

--- ui/src/components/base/BaseButtonClose.tsx
+++ ui/src/components/base/BaseButtonClose.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes, FC } from "react";
+import { ButtonHTMLAttributes, FC } from "react";
 import { useConfig } from "../../Provider";
 import { IconClose } from "../icons/IconClose";
 
