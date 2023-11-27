063c3437207a9f431f84c9c040a478a5ae78b208
Franklin Shera <fshera96@gmail.com>
2023-11-28 00:31:58 +0300

    feat: add form components

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -3,7 +3,11 @@ import {
   BaseHeading,
   BaseLink,
   BaseParagraph,
+  BasePlacehold,
   BaseText,
+  BaseTextarea,
+  BaseThemeSwitch,
+  BaseThemeToggle,
 } from "@shuriken-ui/react";
 
 import Image from "next/image";
@@ -11,22 +15,47 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center   p-24">
-      <BaseParagraph>Base Paragraph Here</BaseParagraph>
-      <BaseHeading as="h1">Semaa</BaseHeading>
-      <BaseAvatar
-        // as={Image}
-        width={300}
-        height={300}
-        size="4xl"
-        dot
-        src="https://picsum.photos/300/300"
-        srcDark="https://picsum.photos/300/300"
-      />
+    <main className="  min-h-screen   grid grid-cols-4  p-24">
+      <div>
+        <BaseThemeToggle disableTransitions inverted />
+        <BaseThemeSwitch disableTransitions />
+      </div>
 
-      <BaseLink as={Link} href={""}>
-        Hi link
-      </BaseLink>
+      <div>
+        <BaseParagraph>Base Paragraph Here</BaseParagraph>
+        <BaseHeading as="h5">Semaa</BaseHeading>
+        <BaseText>BaseText</BaseText>
+        <BaseLink as={Link} href={""}>
+          BaseLink
+        </BaseLink>
+      </div>
+
+      <div>
+        <BaseAvatar
+          // as={Image}
+          width={300}
+          height={300}
+          size="4xl"
+          dot
+          src="https://picsum.photos/300/300"
+          srcDark="https://picsum.photos/300/300"
+        />
+      </div>
+      <div className="flex flex-col gap-10">
+        <BaseTextarea
+          autogrow
+          label="BaseTextarea 1"
+          stateModifiers={{ lazy: true }}
+          placeholder="Write something..."
+        />
+        <BaseTextarea
+          autogrow
+          label="BaseTextarea 2"
+          labelFloat
+          stateModifiers={{ trim: true }}
+          placeholder="Write something..."
+        />
+      </div>
     </main>
   );
 }
--- ui/src/components/form/index.ts
+++
@@ -1,3 +0,0 @@
-/**
- * Export Form Components
- */
