2f59431f184baf33c9af80fa185be9d4f3a4764b
Franklin Shera <fshera96@gmail.com>
2023-12-18 13:29:50 +0300

    fix: refactor useColorMode hook to support system theme preference

--- ui/src/hooks/useColorMode.ts
+++ ui/src/hooks/useColorMode.ts
@@ -1,49 +1,75 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { useMediaQuery } from "./useMediaQuery";
+import { ColorMode } from "../types";
+import { useThemeContext } from "../Provider";
 
 const COLOR_SCHEME_QUERY = "(prefers-color-scheme: dark)";
 
 const THEME_KEY = "nui-theme";
 
-type ColorMode = "dark" | "light";
-
-const setTheme = (value: string) =>
+const setTheme = (value: ColorMode) =>
   window.localStorage.setItem(THEME_KEY, value);
 
 export function useColorMode() {
-  const [colorMode, setColorMode] = useState<ColorMode>("dark");
+  // const [colorMode, setColorMode] = useState<ColorMode>("dark");
+  const { colorMode, setColorMode } = useThemeContext();
 
   const isDarkOS = useMediaQuery(COLOR_SCHEME_QUERY);
 
+  const systemTheme = useMemo(() => (isDarkOS ? "dark" : "light"), [isDarkOS]);
+
+  function toggleClass(color: string) {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.remove("light");
+
+    document.documentElement.classList.add(color);
+  }
+
   function toggle() {
-    setColorMode((prevColorMode) => {
-      const currentColorMode = prevColorMode === "dark" ? "light" : "dark";
+    setColorMode(({ value }) => {
+      const currentColorMode = value === "dark" ? "light" : "dark";
 
       setTheme(currentColorMode);
+      toggleClass(currentColorMode);
 
-      return currentColorMode;
+      return { value: currentColorMode, preference: currentColorMode };
     });
+  }
+
+  function setColors(color: ColorMode) {
+    setTheme(color);
+
+    const preferedColor = color === "system" ? systemTheme : color;
+
+    setColorMode({ value: preferedColor, preference: color });
+
+    toggleClass(preferedColor);
   }
 
   useEffect(() => {
     if (THEME_KEY in localStorage) {
       const localTheme = window.localStorage.getItem(THEME_KEY) as ColorMode;
 
-      setColorMode(localTheme === "dark" ? "dark" : "light");
-    } else {
-      const systemTheme = isDarkOS ? "dark" : "light";
+      if (localTheme === "system") {
+        toggleClass(systemTheme);
+        setColorMode({ value: systemTheme, preference: "system" });
 
-      setTheme(systemTheme);
-      setColorMode(systemTheme);
+        return;
+      }
+
+      const theme = localTheme === "dark" ? "dark" : "light";
+
+      toggleClass(theme);
+      setColorMode({ value: theme, preference: theme });
+    } else {
+      setTheme("system");
+      setColorMode({ value: systemTheme, preference: "system" });
     }
-  }, [isDarkOS]);
+  }, [isDarkOS, setColorMode, systemTheme]);
 
   return {
     colorMode,
-    setColorMode: (color: ColorMode) => {
-      setTheme(color);
-      setColorMode(color);
-    },
+    setColorMode: setColors,
     toggle,
   };
 }
