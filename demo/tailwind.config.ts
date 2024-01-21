3f1cdd16f95c4f90f2eff16abf245a2387571737
Franklin Shera <fshera96@gmail.com>
2024-01-21 23:17:18 +0300

    clean content

--- demo/tailwind.config.ts
+++ demo/tailwind.config.ts
@@ -2,19 +2,12 @@ import { withShurikenUI } from "@shuriken-ui/tailwind";
 
 export default withShurikenUI({
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@shuriken-ui/react/dist/**/*.js",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      shurikenUi: {},
     },
   },
-  plugins: [],
 });
