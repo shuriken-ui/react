7a27f5fe1e4b8dd35cb33e53a91fa56c2d7685f9
Franklin Shera <fshera96@gmail.com>
2024-01-21 23:17:47 +0300

    organize sidebar  data

--- demo/src/app/layout.tsx
+++ demo/src/app/layout.tsx
@@ -4,8 +4,9 @@ import "./globals.css";
 import { ShurikenUIProvider } from "@shuriken-ui/react";
 import ThemeSwitcher from "./ThemeSwitcher";
 import Link from "next/link";
-import LogoLink from "./LogoLink";
-import { routes } from "@/data";
+import NuiLogoText from "./NuiLogoText";
+import LinkIcon from "./LinkIcon";
+import { links } from "@/data/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,64 +28,56 @@ export default function RootLayout({
     <ShurikenUIProvider>
       <html lang="en">
         <body className={inter.className}>
-          <div>
-            <div className="flex min-h-screen">
-              <div className="w-2/12">
-                <nav className="fixed w-2/12 max-h-screen overflow-y-scroll nui-slimscroll">
-                  <LogoLink />
+          <div className="flex min-h-screen">
+            <div className="w-2/12">
+              <nav className="fixed w-2/12 max-h-screen overflow-y-scroll nui-slimscroll">
+                <Link href="/" className="inline-block px-6 py-4">
+                  <NuiLogoText className="h-8 w-auto text-muted-600" />
+                </Link>
 
-                  <ul className="flex flex-col gap-6">
-                    {routes.map((routeGroup, index) => (
-                      <li key={index}>
-                        <h3 className="capitalize text-sm text-muted-400 font-medium border-b border-muted-300 mx-4 pb-2 mb-4">
-                          {routeGroup.title}
-                        </h3>
+                <ul className="flex flex-col gap-10 pt-6">
+                  {links.map((linkGroup, index) => (
+                    <li key={index}>
+                      <h3 className="uppercase text-xs tracking-wider text-muted-400 font-semibold mx-4 px-3 pb-2">
+                        {linkGroup.title}
+                      </h3>
 
-                        <ul>
-                          {routeGroup.links.map((link, index) => (
-                            <li key={index}>
-                              <Link
-                                active-class="text-primary-500"
-                                className="py-2 px-4 hover:bg-muted-50 dark:hover:bg-muted-900 flex flex-col"
-                                href={link.href}
-                              >
-                                <span className="flex gap-1 items-center">
-                                  {/* <Icon
-                        class="text-muted-600"
-                        v-if="
-                          typeof (route as RouteRecordNormalized).meta?.icon ===
-                            'string' &&
-                          (route as RouteRecordNormalized).meta?.icon
-                        "
-                        :name="
-                          (route as RouteRecordNormalized).meta?.icon as string
-                        "
-                      /> */}
-                                  <span className="inline-bloc">
+                      <ul className="px-3">
+                        {linkGroup.links.map((link, index) => (
+                          <li key={index}>
+                            <Link
+                              // active-class="text-primary-500"
+                              className="py-2 px-4 hover:bg-muted-100 dark:hover:bg-muted-900 rounded-lg flex flex-col"
+                              href={link.href}
+                            >
+                              <div className="flex gap-4 items-center">
+                                <LinkIcon icon={link.icon} />
+                                <div>
+                                  <p className="text-sm font-sans text-muted-800 dark:text-muted-100">
                                     {link.title}
-                                  </span>
-                                </span>
-                                {link.description && (
-                                  <span className="italic text-xs text-muted-500">
-                                    {link.description}
-                                  </span>
-                                )}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-              </div>
-              <div className="w-10/12 border-l border-muted-200 dark:border-muted-800">
-                {children}
-              </div>
+                                  </p>
+                                  {link.description && (
+                                    <p className="text-xs text-muted-500">
+                                      {link.description}
+                                    </p>
+                                  )}
+                                </div>
+                              </div>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
             </div>
-            <div className="fixed top-0 end-0 pr-6 pt-6 z-50">
-              <ThemeSwitcher />
+            <div className="w-10/12 border-l border-muted-200 dark:border-muted-800">
+              {children}
             </div>
+          </div>
+          <div className="fixed top-0 end-0 pr-6 pt-6 z-50">
+            <ThemeSwitcher />
           </div>
         </body>
       </html>
--- demo/src/data/sidebar.ts
+++ demo/src/data/sidebar.ts
@@ -1,17 +1,25 @@
-export type PageLink = { title: string; description?: string; href: string };
+export type PageLink = {
+  icon?: string;
+  title: string;
+  description: string;
+  href: string;
+};
 
 export type LinkGroup = { title: string; links: PageLink[] };
 
-export const routes: LinkGroup[] = [
+/** sidebar navigation links */
+export const links: LinkGroup[] = [
   {
     title: "Authority",
     links: [
       {
+        icon: "system-uicons:venn",
         title: "Colors",
         href: "/tests/authority/colors",
         description: "Basic colors",
       },
       {
+        icon: "system-uicons:paragraph-left",
         title: "Typography",
         href: "/tests/authority/typography",
         description: "Tailwind typography plugin",
@@ -22,41 +30,49 @@ export const routes: LinkGroup[] = [
     title: "Base",
     links: [
       {
+        icon: "system-uicons:chain",
         title: "Breadcrumb",
         href: "/tests/base/breadcrumb",
         description: "Breadcrumb component",
       },
       {
+        icon: "system-uicons:duplicate-alt",
         title: "Button Action",
         href: "/tests/base/button-action",
         description: "Button variations",
       },
       {
+        icon: "system-uicons:cross-circle",
         title: "Button Close",
         href: "/tests/base/button-close",
         description: "Button Close variations",
       },
       {
+        icon: "system-uicons:plus-circle",
         title: "Button Icon",
         href: "/tests/base/button-icon",
         description: "Button Icon variations",
       },
       {
+        icon: "system-uicons:duplicate-alt",
         title: "Buttons",
         href: "/tests/base/buttons",
         description: "Button variations",
       },
       {
+        icon: "system-uicons:panel-top",
         title: "Card",
         href: "/tests/base/card",
         description: "SVG icons",
       },
       {
+        icon: "system-uicons:maximise",
         title: "Dropdown",
         href: "/tests/base/dropdown",
         description: "Dropdown examples",
       },
       {
+        icon: "system-uicons:nut",
         title: "Icon Box",
         href: "/tests/base/icon-box",
         description: "Box wrappers for icons",
@@ -67,11 +83,13 @@ export const routes: LinkGroup[] = [
         description: "Highlight text with a background color.",
       },
       {
+        icon: "system-uicons:postcard",
         title: "Message",
         href: "/tests/base/message",
         description: "Base message component",
       },
       {
+        icon: "system-uicons:circle-menu",
         title: "Pagination",
         href: "/tests/base/pagination",
         description: "SVG icons",
@@ -87,16 +105,19 @@ export const routes: LinkGroup[] = [
         description: "SVG icons",
       },
       {
+        icon: "system-uicons:gift",
         title: "Snack",
         href: "/tests/base/snack",
         description: "Snack details",
       },
       {
+        icon: "system-uicons:tags",
         title: "Tag",
         href: "/tests/base/tag",
         description: "Tag variations",
       },
       {
+        icon: "system-uicons:component-add",
         title: "Tooltips",
         href: "/tests/base/tooltips",
         description: "Add a tooltip to any element.",
@@ -107,11 +128,13 @@ export const routes: LinkGroup[] = [
     title: "Form",
     links: [
       {
+        icon: "system-uicons:checkbox-checked",
         title: "Checkbox",
         href: "/tests/form/checkbox",
         description: "Basic custom checkbox and radio",
       },
       {
+        icon: "system-uicons:calendar-move",
         title: "Input",
         href: "/tests/form/input",
         description:
@@ -123,11 +146,13 @@ export const routes: LinkGroup[] = [
         description: "Progress component",
       },
       {
+        icon: "system-uicons:record",
         title: "Radio",
         href: "/tests/form/radio",
         description: "SVG icons",
       },
       {
+        icon: "system-uicons:toggle",
         title: "Switch",
         href: "/tests/form/switch",
         description: "SVG icons",
