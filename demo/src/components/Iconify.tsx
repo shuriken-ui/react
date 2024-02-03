8ecaa4fc4a0829c8101ced509d3ad529a95d6430
Franklin Shera <fshera96@gmail.com>
2024-02-03 23:47:39 +0300

    add Iconify wrapper

---
+++ demo/src/components/Iconify.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Icon, IconProps } from "@iconify/react/dist/iconify.js";
+import React, { FC } from "react";
+
+const Iconify: FC<IconProps> = (props) => {
+  return <Icon {...props} />;
+};
+
+export default Iconify;
