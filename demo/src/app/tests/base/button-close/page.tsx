dc3ebfe2dff677bc9d351b80fd6198bc3425ff3d
Franklin Shera <fshera96@gmail.com>
2024-02-04 00:56:17 +0300

    update button close demo

--- demo/src/app/tests/base/button-close/page.tsx
+++ demo/src/app/tests/base/button-close/page.tsx
@@ -1,53 +1,109 @@
-import { BaseButtonClose, BaseHeading } from "@shuriken-ui/react";
+import { BaseButtonClose } from "@shuriken-ui/react";
 import { Metadata } from "next";
 import React from "react";
+import { getMeta } from "@/data/sidebar";
+import NuiPreview from "@/components/NuiPreview";
+import NuiPreviewContainer from "@/components/NuiPreviewContainer";
+
+const meta = getMeta("base", "buttonClose");
 
 export const metadata: Metadata = {
-  title: "Button Close",
-  description: "Button Close variations",
+  ...meta,
 };
 
-function ButtonClosePage() {
+export default function ButtonClosePage() {
   return (
-    <div className="flex flex-col gap-12 [&>*]:p-8 [&>:nth-child(odd)]:bg-muted-100 dark:[&>:nth-child(odd)]:bg-muted-900 pb-32">
-      <div>
-        <BaseHeading size="xl" weight="medium" className="mb-10">
-          Button shape
-        </BaseHeading>
-        <div className="flex gap-2">
-          <BaseButtonClose color="primary" shape="straight">
-            Button
-          </BaseButtonClose>
-          <BaseButtonClose color="primary" shape="rounded">
-            Button
-          </BaseButtonClose>
-          <BaseButtonClose color="primary" shape="smooth">
-            Button
-          </BaseButtonClose>
-          <BaseButtonClose color="primary" shape="curved">
-            Button
-          </BaseButtonClose>
-          <BaseButtonClose color="primary" shape="full">
-            Button
-          </BaseButtonClose>
+    <NuiPreviewContainer title={meta.title}>
+      <NuiPreview title="Radius" description="Button close component radiuses">
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="md" rounded="none" color="muted" />
+          <BaseButtonClose size="md" rounded="sm" color="muted" />
+          <BaseButtonClose size="md" rounded="md" color="muted" />
+          <BaseButtonClose size="md" rounded="lg" color="muted" />
+          <BaseButtonClose size="md" rounded="full" color="muted" />
         </div>
-      </div>
-      <div>
-        <BaseHeading size="xl" weight="medium" className="mb-10">
-          Button colors
-        </BaseHeading>
-        <div className="flex gap-2">
-          <BaseButtonClose>Button</BaseButtonClose>
-          <BaseButtonClose color="muted">Button</BaseButtonClose>
-          <BaseButtonClose color="primary">Button</BaseButtonClose>
-          <BaseButtonClose color="success">Button</BaseButtonClose>
-          <BaseButtonClose color="info">Button</BaseButtonClose>
-          <BaseButtonClose color="warning">Button</BaseButtonClose>
-          <BaseButtonClose color="danger">Button</BaseButtonClose>
+      </NuiPreview>
+
+      <NuiPreview title="Sizes" description="Button close component sizes">
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="md" color="muted" />
+          <BaseButtonClose size="sm" rounded="md" color="muted" />
+          <BaseButtonClose size="md" rounded="md" color="muted" />
+          <BaseButtonClose size="lg" rounded="md" color="muted" />
         </div>
-      </div>
-    </div>
+      </NuiPreview>
+
+      <NuiPreview
+        title="Color: default"
+        description="Button close component default color"
+      >
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="full" color="default" />
+          <BaseButtonClose size="sm" rounded="full" color="default" />
+          <BaseButtonClose size="md" rounded="full" color="default" />
+          <BaseButtonClose size="lg" rounded="full" color="default" />
+        </div>
+      </NuiPreview>
+
+      <NuiPreview
+        title="Color: muted"
+        description="Button close component muted color"
+      >
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="full" color="muted" />
+          <BaseButtonClose size="sm" rounded="full" color="muted" />
+          <BaseButtonClose size="md" rounded="full" color="muted" />
+          <BaseButtonClose size="lg" rounded="full" color="muted" />
+        </div>
+      </NuiPreview>
+
+      <NuiPreview
+        title="Color: info"
+        description="Button close component info color"
+      >
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="full" color="info" />
+          <BaseButtonClose size="sm" rounded="full" color="info" />
+          <BaseButtonClose size="md" rounded="full" color="info" />
+          <BaseButtonClose size="lg" rounded="full" color="info" />
+        </div>
+      </NuiPreview>
+
+      <NuiPreview
+        title="Color: success"
+        description="Button close component success color"
+      >
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="full" color="success" />
+          <BaseButtonClose size="sm" rounded="full" color="success" />
+          <BaseButtonClose size="md" rounded="full" color="success" />
+          <BaseButtonClose size="lg" rounded="full" color="success" />
+        </div>
+      </NuiPreview>
+
+      <NuiPreview
+        title="Color: warning"
+        description="Button close component warning color"
+      >
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="full" color="warning" />
+          <BaseButtonClose size="sm" rounded="full" color="warning" />
+          <BaseButtonClose size="md" rounded="full" color="warning" />
+          <BaseButtonClose size="lg" rounded="full" color="warning" />
+        </div>
+      </NuiPreview>
+
+      <NuiPreview
+        title="Color: danger"
+        description="Button close component danger color"
+      >
+        <div className="flex flex-wrap items-end gap-2">
+          <BaseButtonClose size="xs" rounded="full" color="danger" />
+          <BaseButtonClose size="sm" rounded="full" color="danger" />
+          <BaseButtonClose size="md" rounded="full" color="danger" />
+          <BaseButtonClose size="lg" rounded="full" color="danger" />
+        </div>
+      </NuiPreview>
+    </NuiPreviewContainer>
   );
 }
-
-export default ButtonClosePage;
