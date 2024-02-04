b537a47b5d6d759579bdca02927192a3236fc052
Franklin Shera <fshera96@gmail.com>
2024-02-04 14:57:22 +0300

    update message demo

--- demo/src/app/tests/base/message/page.tsx
+++ demo/src/app/tests/base/message/page.tsx
@@ -2,86 +2,59 @@ import { BaseHeading, BaseMessage } from "@shuriken-ui/react";
 import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "Message",
-  description: "Base message component",
-};
+import { getMeta } from "@/data/sidebar";
+import NuiPreviewContainer from "@/components/NuiPreviewContainer";
+import NuiPreview from "@/components/NuiPreview";
 
+const meta = getMeta("base", "message");
+
+export const metadata: Metadata = {
+  ...meta,
+};
 function MessagePage() {
   return (
-    <div className="flex flex-col gap-12 [&>*]:p-8 [&>:nth-child(odd)]:bg-muted-100 dark:[&>:nth-child(odd)]:bg-muted-900 pb-32">
-      <div>
-        <div>
-          <BaseHeading size="xl" weight="medium" className="mb-10">
-            Message Shape
-          </BaseHeading>
+    <NuiPreviewContainer title={meta.title}>
+      <NuiPreview title="Type" description="Message component types">
+        <div className="grid grid-cols-1 md:grid-cols-2 max-w-2xl gap-4">
+          <BaseMessage>A successful message.</BaseMessage>
+          <BaseMessage type="info">An information message.</BaseMessage>
+          <BaseMessage type="warning">A warning message.</BaseMessage>
+          <BaseMessage type="danger">A destructive message.</BaseMessage>
         </div>
-        <div className="grid grid-cols-5 gap-2">
-          <BaseMessage
-            type="muted"
-            shape="straight"
-            message="A muted message ..."
-            closable
-          />
-          <BaseMessage
-            type="default"
-            shape="rounded"
-            message="A default message ..."
-            closable
-          />
-          <BaseMessage
-            type="primary"
-            shape="smooth"
-            message="A primary message ..."
-            closable
-          />
-          <BaseMessage
-            type="info"
-            shape="curved"
-            message="An info message ..."
-            closable
-          />
-          <BaseMessage
-            type="success"
-            shape="full"
-            message="A success message ..."
-            closable
-          />
+      </NuiPreview>
+
+      <NuiPreview title="Icon" description="Message component icons">
+        <div className="grid grid-cols-1 md:grid-cols-2 max-w-2xl gap-4">
+          <BaseMessage icon>A successful message.</BaseMessage>
+          <BaseMessage type="info" icon>
+            An information message.
+          </BaseMessage>
+          <BaseMessage type="warning" icon>
+            A warning message.
+          </BaseMessage>
+          <BaseMessage type="danger" icon>
+            A destructive message.
+          </BaseMessage>
         </div>
-      </div>
-      <div>
-        <div>
-          <BaseHeading size="xl" weight="medium" className="mb-10">
-            Message type
-          </BaseHeading>
+      </NuiPreview>
+
+      <NuiPreview title="Radius" description="Message component radiuses">
+        <div className="grid grid-cols-1 md:grid-cols-2 max-w-2xl gap-4">
+          <BaseMessage icon rounded="sm">
+            A successful message.
+          </BaseMessage>
+          <BaseMessage type="info" icon rounded="md">
+            An information message.
+          </BaseMessage>
+          <BaseMessage type="warning" icon rounded="lg">
+            A warning message.
+          </BaseMessage>
+          <BaseMessage type="danger" icon rounded="full">
+            A destructive message.
+          </BaseMessage>
         </div>
-        <div className="grid grid-cols-7 gap-2">
-          <BaseMessage
-            type="default"
-            message="A default message ..."
-            closable
-          />
-          <BaseMessage type="muted" message="A muted message ..." closable />
-          <BaseMessage
-            type="primary"
-            message="A primary message ..."
-            closable
-          />
-          <BaseMessage type="info" message="An info message ..." closable />
-          <BaseMessage
-            type="success"
-            message="A success message ..."
-            closable
-          />
-          <BaseMessage
-            type="warning"
-            message="A warning message ..."
-            closable
-          />
-          <BaseMessage type="danger" message="A danger message ..." closable />
-        </div>
-      </div>
-    </div>
+      </NuiPreview>
+    </NuiPreviewContainer>
   );
 }
 
