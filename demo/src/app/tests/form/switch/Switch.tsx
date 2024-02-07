88802e6ba35c7d710ce5053df9c198fe9596a85b
Franklin Shera <fshera96@gmail.com>
2024-02-07 12:15:00 +0300

    update switch demo

--- demo/src/app/tests/form/switch/Switch.tsx
+++ demo/src/app/tests/form/switch/Switch.tsx
@@ -1,29 +1,141 @@
 "use client";
 import {
-  BaseHeading,
   BaseSwitchBall,
   BaseSwitchThin,
-  BaseThemeSwitch,
-  BaseThemeToggle,
+  BaseFocusLoop,
 } from "@shuriken-ui/react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import NuiPreview from "@/components/NuiPreview";
 
 const Switch = () => {
-  const [ball, setBall] = useState(false);
+  const [thinValues, setThinValues] = useState({
+    primary: true,
+    info: true,
+    success: true,
+    warning: true,
+    danger: true,
+  });
+
+  const [ballValues, setBallValues] = useState({
+    primary: true,
+    info: true,
+    success: true,
+    warning: true,
+    danger: true,
+  });
+
   return (
-    <div className="flex flex-col gap-12 [&>*]:p-8 [&>:nth-child(odd)]:bg-muted-100 dark:[&>:nth-child(odd)]:bg-muted-900 pb-32">
-      <div>
-        <BaseHeading size="xl" weight="medium" className="mb-10">
-          Switch
-        </BaseHeading>
-        <div className="flex items-center gap-8" dir="rtl">
-          <BaseThemeSwitch />
-          <BaseThemeToggle />
-          <BaseSwitchBall checked={ball} onChange={(value) => setBall(value)} />
-          <BaseSwitchThin checked={ball} onChange={(value) => setBall(value)} />
+    <Fragment>
+      <NuiPreview title="Thin: default" description="Switch thin component">
+        <BaseFocusLoop className="grid grid-cols-2 gap-6 md:max-w-lg md:grid-cols-4">
+          <BaseSwitchThin
+            onChange={(value) =>
+              setThinValues((oldValue) => ({ ...oldValue, primary: value }))
+            }
+            checked={thinValues.primary}
+            label="Primary"
+            color="primary"
+          />
+          <BaseSwitchThin
+            onChange={(value) =>
+              setThinValues((oldValue) => ({ ...oldValue, info: value }))
+            }
+            checked={thinValues.info}
+            label="Info"
+            color="info"
+          />
+          <BaseSwitchThin
+            onChange={(value) =>
+              setThinValues((oldValue) => ({ ...oldValue, success: value }))
+            }
+            checked={thinValues.success}
+            label="Success"
+            color="success"
+          />
+          <BaseSwitchThin
+            onChange={(value) =>
+              setThinValues((oldValue) => ({ ...oldValue, warning: value }))
+            }
+            checked={thinValues.warning}
+            label="Warning"
+            color="warning"
+          />
+          <BaseSwitchThin
+            onChange={(value) =>
+              setThinValues((oldValue) => ({ ...oldValue, danger: value }))
+            }
+            checked={thinValues.danger}
+            label="Danger"
+            color="danger"
+          />
+        </BaseFocusLoop>
+      </NuiPreview>
+
+      <NuiPreview title="Thin: sublabel" description="Switch thin sublabel">
+        <div className="grid gap-6 md:max-w-lg md:grid-cols-2">
+          <BaseSwitchThin
+            label="Primary"
+            sublabel="This is a primary switch"
+            color="primary"
+          />
         </div>
-      </div>
-    </div>
+      </NuiPreview>
+
+      <NuiPreview title="Ball: default" description="Switch thin component">
+        <BaseFocusLoop className="grid grid-cols-2 gap-6 md:max-w-lg md:grid-cols-4">
+          <BaseSwitchBall
+            onChange={(value) =>
+              setBallValues((oldValue) => ({ ...oldValue, primary: value }))
+            }
+            checked={ballValues.primary}
+            label="Primary"
+            color="primary"
+          />
+          <BaseSwitchBall
+            onChange={(value) =>
+              setBallValues((oldValue) => ({ ...oldValue, info: value }))
+            }
+            checked={ballValues.info}
+            label="Info"
+            color="info"
+          />
+          <BaseSwitchBall
+            onChange={(value) =>
+              setBallValues((oldValue) => ({ ...oldValue, success: value }))
+            }
+            checked={ballValues.success}
+            label="Success"
+            color="success"
+          />
+          <BaseSwitchBall
+            onChange={(value) =>
+              setBallValues((oldValue) => ({ ...oldValue, warning: value }))
+            }
+            checked={ballValues.warning}
+            label="Warning"
+            color="warning"
+          />
+          <BaseSwitchBall
+            onChange={(value) =>
+              setBallValues((oldValue) => ({ ...oldValue, danger: value }))
+            }
+            checked={ballValues.danger}
+            label="Danger"
+            color="danger"
+          />
+        </BaseFocusLoop>
+      </NuiPreview>
+
+      <NuiPreview title="Ball: sublabel" description="Switch thin sublabel">
+        <div className="grid gap-6 md:max-w-lg md:grid-cols-2">
+          <BaseSwitchBall
+            label="Primary"
+            sublabel="This is a primary switch"
+            color="primary"
+          />
+        </div>
+      </NuiPreview>
+    </Fragment>
   );
 };
 
--- demo/src/app/tests/form/switch/page.tsx
+++ demo/src/app/tests/form/switch/page.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Switch from "./Switch";
 import { Metadata } from "next";
+import NuiPreviewContainer from "@/components/NuiPreviewContainer";
+
+import { getMeta } from "@/data/sidebar";
+
+const meta = getMeta("form", "switch");
+
 export const metadata: Metadata = {
-  title: "Switch",
-  description: "SVG icons",
+  ...meta,
 };
+
 function SwitchPage() {
-  return <Switch />;
+  return (
+    <NuiPreviewContainer title={meta.title}>
+      <Switch />
+    </NuiPreviewContainer>
+  );
 }
 
 export default SwitchPage;
